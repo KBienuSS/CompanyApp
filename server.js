e80ce7290282f4cbea88e82c126ec899511c7af3
Kamil <bienius.234@gmail.com>
2026-05-25 20:10:39 +0200

    Testing endpoints

--- server.js
+++ server.js
@@ -6,36 +6,39 @@ const employeesRoutes = require('./routes/employees.routes');
 const departmentsRoutes = require('./routes/departments.routes');
 const productsRoutes = require('./routes/products.routes');
 
+const NODE_ENV = process.env.NODE_ENV;
+let dbUri = '';
+
+if(NODE_ENV === 'production') dbUri = 'url to remote db';
+else if(NODE_ENV === 'test') dbUri = 'mongodb://127.0.0.1:27017/companyDBtest';
+else dbUri = 'mongodb://127.0.0.1:27017/companyDB';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api', employeesRoutes);
+app.use('/api', departmentsRoutes);
+app.use('/api', productsRoutes);
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Not found...' });
+});
+
 async function startServer() {
   try {
-    mongoose.connect('mongodb://127.0.0.1:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(dbUri);
     console.log('Successfully connected to the database');
-    const app = express();
-    const db = mongoose.connection;
-
-    app.use(cors());
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-
-    app.use((req, res, next) => {
-      req.db = db;
-      next();
-    });
-    
-    app.use('/api', employeesRoutes);
-    app.use('/api', departmentsRoutes);
-    app.use('/api', productsRoutes);
-
-    app.use((req, res) => {
-      res.status(404).send({ message: 'Not found...' });
-    });
-
     app.listen(8000, () => {
       console.log('Server is running on port: 8000');
     });
-  } catch (err) {
+  } catch(err) {
     console.log('Błąd połączenia:', err);
   }
 }
 
 startServer();
+
+module.exports = app;
